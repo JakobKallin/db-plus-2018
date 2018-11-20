4b86b9860cd018ded6967dc117b1abe2f918ba03
Jakob Kallin <jakob@jakobkallin.com>
2018-11-20 08:00:29 +0100

    Session 2

---
+++ Session2.sql
@@ -0,0 +1,49 @@
+-- Skriv SQL-uttryck som hämtar följande data från er musikdatabas:
+
+-- 1. Alla låtar sorterade på titel, A-Z.
+SELECT ...;
+
+-- 2. Alla låtar sorterade på längd, längst först.
+SELECT ...;
+
+-- 3. Alla låtar längre än 4 minuter.
+SELECT ...;
+
+-- 4. Alla låtar mellan 3 och 4 minuter långa.
+SELECT ...;
+
+-- 5. Alla låtar som INTE är mellan 3 och 4 minuter långa.
+SELECT ...;
+
+-- 6. Alla låtar vars text är längre än 1.000 tecken.
+SELECT ...;
+
+-- 7. Alla låtar vars titel börjar på 'T'.
+SELECT ...;
+
+-- 8. Alla låtar vars text innehåller ordet 'love'.
+SELECT ...;
+
+-- 9. Alla album släppta efter 2000.
+SELECT ...;
+
+-- 10. Alla album släppta i juni.
+SELECT ...;
+
+-- 11. Alla band från Sverige.
+SELECT ...;
+
+-- 12. Alla band som inte är från Sverige.
+SELECT ...;
+
+-- 13. Alla band från USA eller Kanada.
+SELECT ...;
+
+-- 14. Alla band och hur många år de har varit verksamma. (Med andra ord: "Hur gammalt är bandet?")
+SELECT ...;
+
+-- 15. Alla låtar, med längden angiven i hela minuter (avrundat till närmaste heltal).
+SELECT ...;
+
+-- 16. Alla låtar, med texten förkortad till max 100 tecken och följt av '...' om den är längre.
+SELECT ...;
